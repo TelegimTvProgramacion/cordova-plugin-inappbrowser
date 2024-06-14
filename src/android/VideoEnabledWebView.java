6b22011d6821c7faccd747156b3b69790f655f2b
TelegimTV Programacion <programacion@telegim.tv>
2024-06-14 18:01:10 +0200

    Update VideoEnabledWebView.java

--- src/android/VideoEnabledWebView.java
+++ src/android/VideoEnabledWebView.java
@@ -47,27 +47,23 @@ public class VideoEnabledWebView extends WebView
     }
 
     private VideoEnabledWebChromeClient videoEnabledWebChromeClient;
-    private boolean addedJavascriptInterface;
-
+    
     @SuppressWarnings("unused")
     public VideoEnabledWebView(Context context)
     {
         super(context);
-        addedJavascriptInterface = false;
     }
 
     @SuppressWarnings("unused")
     public VideoEnabledWebView(Context context, AttributeSet attrs)
     {
         super(context, attrs);
-        addedJavascriptInterface = false;
     }
 
     @SuppressWarnings("unused")
     public VideoEnabledWebView(Context context, AttributeSet attrs, int defStyle)
     {
         super(context, attrs, defStyle);
-        addedJavascriptInterface = false;
     }
 
     /**
@@ -99,41 +95,25 @@ public class VideoEnabledWebView extends WebView
     @Override
     public void loadData(String data, String mimeType, String encoding)
     {
-        addJavascriptInterface();
         super.loadData(data, mimeType, encoding);
     }
 
     @Override
     public void loadDataWithBaseURL(String baseUrl, String data, String mimeType, String encoding, String historyUrl)
     {
-        addJavascriptInterface();
         super.loadDataWithBaseURL(baseUrl, data, mimeType, encoding, historyUrl);
     }
 
     @Override
     public void loadUrl(String url)
     {
-        addJavascriptInterface();
         super.loadUrl(url);
     }
 
     @Override
     public void loadUrl(String url, Map<String, String> additionalHttpHeaders)
     {
-        addJavascriptInterface();
         super.loadUrl(url, additionalHttpHeaders);
-    }
-
-    private void addJavascriptInterface()
-    {
-        if (!addedJavascriptInterface)
-        {
-            // Add javascript interface to be called when the video ends (must be done before page load)
-            //noinspection all
-            addJavascriptInterface(new JavascriptInterface(), "_VideoEnabledWebView"); // Must match Javascript interface name of VideoEnabledWebChromeClient
-
-            addedJavascriptInterface = true;
-        }
     }
 
 }
